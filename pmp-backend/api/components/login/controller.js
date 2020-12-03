9e99b69f7469a90f99f00f4680a1576abbdf0b78
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-12-03 01:58:36 -0600

    Creating/one end point for login

--- pmp-backend/api/components/login/controller.js
+++ pmp-backend/api/components/login/controller.js
@@ -3,11 +3,14 @@
 const { nanoid } = require('nanoid')
 const bcrypt = require('bcrypt')
 const config = require('../../../../config/config')
+const trelloAuth = require('../../../utils/auth/trello/index')
+const boom = require('@hapi/boom')
+const jwt = require('jsonwebtoken')
 
 module.exports = (store) => {
+  
   const upsert = async (data) => {
     try {
-      console.log(data)
       const { users, table } = await store(config(false))
       const userExist = await users.findByNickname(data.nickname)
       const newUser = {
@@ -55,13 +58,75 @@ module.exports = (store) => {
     }
   }
 
-  const login = (body) => {
-    console.log(body)
-    return body
+  const infoUser = async (info) => {
+    try {
+      const query = {
+        appKey: config(false).trelloId,
+        appSecret: config(false).trelloSecret,
+        callbackUrl: config(false).apiUrl
+      }
+      const tokenKeyPair = {
+        accessToken: info,
+        accessTokenSecrete: config(false).trelloSecret
+      }
+      const data = JSON.parse(await trelloAuth(query).getUserInfo(tokenKeyPair))
+      const user = {
+        nickname: data.username,
+        email: data.email,
+        password: data.id,
+        rol: data.memberType,
+        trelloIdUser: tokenKeyPair.accessTokenSecrete,
+        token: tokenKeyPair.accessToken
+      }
+      if (user.email === null) user.email = `${data.username}@trello.com`
+      const userSaved = await upsert(user)
+      userSaved.data = data
+      return userSaved
+    } catch (err) {
+      throw new Error(err)
+    }
+  }
+
+  const login = async (req) => {
+    const userInfo = await infoUser(req.token)
+    const apiKeyToken = config(false).apiKeyToken 
+    if (!apiKeyToken) next(boom.unauthorized('apiKeyToken is required'))
+    try {
+      if (!userInfo) next(boom.unauthorized(error))
+      const { apiKey } = await store(config(false))
+      const ApiKey = await apiKey.findByToken(apiKeyToken)
+      if (!ApiKey) next(boom.unauthorized())
+
+      const Nickname = userInfo.Nickname
+      const uuid = userInfo.uuid
+      const email = userInfo.email
+      const tokenUser = req.token
+      const rol = userInfo.rol
+      const payload = {
+        uuid,
+        Nickname,
+        email,
+        tokenUser,
+        rol,
+        scopes: ApiKey.scopes,
+        data: userInfo.data
+      }
+      const token = jwt.sign(payload, config(false).authJwtSecret, {
+        expiresIn: '12h'
+      })
+      delete userInfo.data.id
+      const data = {
+        user: userInfo,
+        token: token
+      }
+
+      return data
+    } catch (err) {
+      throw new Error(err)
+    }
   }
 
   return {
-    upsert,
     login
   }
 }
--- pmp-backend/api/components/login/network.js
+++ pmp-backend/api/components/login/network.js
@@ -1,72 +1,19 @@
 'use strict'
 
 const express = require('express')
-const passport = require('passport')
 const response = require('../../../network/response')
 
 const controller = require('./index')
 const router = express.Router()
 
-const config = require('../../../../config/config')
-const boom = require('@hapi/boom')
-const jwt = require('jsonwebtoken')
-const store = require('../../../../pmp-db/index')
-const utils = require('../../../../pmp-db/utils/index')
-
-require('../../../utils/auth/strategies/basic')
-
 const postSignIn = async (req, res, next) => {
-  const apiKeyToken = req.params
-  if (!apiKeyToken) next(boom.unauthorized('apiKeyToken is required'))
-  passport.authenticate('basic', (error, user) => {
-    try {
-      if (error || !user) next(boom.unauthorized())
-      req.login(user, { session: false }, async (error) => {
-        const { apiKey } = await store(config(false)).catch(utils.handleFatalError)
-
-        if (error) next(error)
-        const ApiKey = await apiKey.findByToken(apiKeyToken.apiKeyToken)
-        if (!ApiKey) next(boom.unauthorized())
-        const { Nickname } = user
-        const uuid = user.uuid
-        const email = user.email
-        const tokenUser = user.token
-        const rol = user.rol
-
-        const payload = {
-          uuid,
-          Nickname,
-          email,
-          tokenUser,
-          rol,
-          scopes: ApiKey.scopes
-        }
-        const token = jwt.sign(payload, config(false).authJwtSecret, {
-          expiresIn: '12h'
-        })
-
-        delete user.token
-        const data = {
-          user: user,
-          token: token
-        }
-        return response.success(req, res, data, 200)
-      })
-    } catch (err) {
-      next(err)
-    }
-  })(req, res, next)
-}
-
-const upsert = (req, res, next) => {
-  controller.upsert(req.body)
+  controller.login(req.params) 
     .then((user) => {
       response.success(req, res, user, 201)
     })
     .catch(next)
 }
 
-router.post('/sign-in/:apiKeyToken', postSignIn)
-router.post('/', upsert)
+router.get('/sign-in/:token', postSignIn)
 
 module.exports = router
