77513fd3acc675b307db6a06315edc505183f871
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-12-13 16:00:56 -0600

    Modify/typos

--- pmp-backend/api/components/labels/controller.js
+++ pmp-backend/api/components/labels/controller.js
@@ -7,7 +7,7 @@ module.exports = (store) => {
   const query = utils().queryFn()
 
   const getALabel = async (body, params) => {
-    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    const tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
     tokenKeyPair.id = params.id
     const boards = JSON.parse(await trelloAuth(query).getALabel(tokenKeyPair))
     return boards
