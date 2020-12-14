abb85caaff4b2aa82a58368ac6d50d7fb0b98ee4
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-12-14 04:31:01 -0600

    Creating/notifications endpoints

--- pmp-backend/api/components/members/index.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const store = require('../../../../pmp-db/index')
-const ctrl = require('./controller')
-
-module.exports = ctrl(store)
--- pmp-backend/api/components/notifications/controller.js
+++ pmp-backend/api/components/notifications/controller.js
@@ -4,15 +4,72 @@ const trelloAuth = require('../../../utils/auth/trello/index')
 const utils = require('../../../utils/utils')
 
 module.exports = (store) => {
-  const idUserInfoById = async (body, params) => {
-    try {
 
-    } catch (e) {
-      throw new Error(e)
-    }
+  const getANotification = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const boards = JSON.parse(await trelloAuth(query).getANotification(tokenKeyPair))
+    return boards
+  }
+
+  const getAFieldOfANotification = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    tokenKeyPair.field = params.field
+    const boards = JSON.parse(await trelloAuth(query).getAFieldOfANotification(tokenKeyPair))
+    return boards
+  }
+
+  const getTheBoardANotificationIsOn = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const boards = JSON.parse(await trelloAuth(query).getTheBoardANotificationIsOn(tokenKeyPair))
+    return boards
+  }
+
+  const getTheCardANotificationIsOn = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const boards = JSON.parse(await trelloAuth(query).getTheCardANotificationIsOn(tokenKeyPair))
+    return boards
+  }
+
+  const getTheListANotificationIsOn = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const boards = JSON.parse(await trelloAuth(query).getTheListANotificationIsOn(tokenKeyPair))
+    return boards
+  }
+
+  const getTheMemberANotificationIsAbout = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const boards = JSON.parse(await trelloAuth(query).getTheMemberANotificationIsAbout(tokenKeyPair))
+    return boards
+  }
+
+  const getTheMemberWhoCreatedTheNotification = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const boards = JSON.parse(await trelloAuth(query).getTheMemberWhoCreatedTheNotification(tokenKeyPair))
+    return boards
+  }
+
+  const getANotificationAssociatedOrganization = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const boards = JSON.parse(await trelloAuth(query).getANotificationAssociatedOrganization(tokenKeyPair))
+    return boards
   }
 
   return {
-    idUserInfo
+    getANotification,
+    getAFieldOfANotification,
+    getTheBoardANotificationIsOn,
+    getTheCardANotificationIsOn,
+    getTheListANotificationIsOn,
+    getTheMemberANotificationIsAbout,
+    getTheMemberWhoCreatedTheNotification,
+    getANotificationAssociatedOrganization
   }
 }
