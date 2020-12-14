a3975b79e8fedf091c5fec2dc1466dc19c126494
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-12-14 03:36:00 -0600

    Creating/checklist endpoints

--- pmp-backend/api/components/checklist/controller.js
+++ pmp-backend/api/components/checklist/controller.js
@@ -4,15 +4,56 @@ const trelloAuth = require('../../../utils/auth/trello/index')
 const utils = require('../../../utils/utils')
 
 module.exports = (store) => {
-  const idUserInfoById = async (body, params) => {
-    try {
 
-    } catch (e) {
-      throw new Error(e)
-    }
+  const getAChecklist = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const boards = JSON.parse(await trelloAuth(query).getAChecklist(tokenKeyPair))
+    return boards
+  }
+
+  const getFieldOnAChecklist = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    tokenKeyPair.field = params.field
+    const boards = JSON.parse(await trelloAuth(query).getFieldOnAChecklist(tokenKeyPair))
+    return boards
+  }
+
+  const getTheBoardTheChecklistIsOn = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const boards = JSON.parse(await trelloAuth(query).getTheBoardTheChecklistIsOn(tokenKeyPair))
+    return boards
+  }
+
+  const getTheCardAChecklistIsOn = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const boards = JSON.parse(await trelloAuth(query).getTheCardAChecklistIsOn(tokenKeyPair))
+    return boards
+  }
+
+  const getCheckitemsOnAChecklist = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const boards = JSON.parse(await trelloAuth(query).getCheckitemsOnAChecklist(tokenKeyPair))
+    return boards
+  }
+
+  const getACheckitemOnAChecklist = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const boards = JSON.parse(await trelloAuth(query).getACheckitemOnAChecklist(tokenKeyPair))
+    return boards
   }
 
   return {
-    idUserInfo
+    getAChecklist,
+    getFieldOnAChecklist,
+    getTheBoardTheChecklistIsOn,
+    getTheCardAChecklistIsOn,
+    getCheckitemsOnAChecklist,
+    getACheckitemOnAChecklist
   }
 }
--- pmp-backend/api/components/checklist/network.js
+++ pmp-backend/api/components/checklist/network.js
@@ -9,15 +9,66 @@ const router = express.Router()
 
 require('../../../utils/auth/strategies/jwt')
 
-const idUserInfoById = async (req, res, next) => {
+const getAChecklist = async (req, res, next) => {
   try {
+    const action = await controller.getAChecklist(req.user, req.params)
+    response.success(req, res, action, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
 
+const getFieldOnAChecklist = async (req, res, next) => {
+  try {
+    const action = await controller.getFieldOnAChecklist(req.user, req.params)
+    response.success(req, res, action, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
+const getTheBoardTheChecklistIsOn = async (req, res, next) => {
+  try {
+    const action = await controller.getTheBoardTheChecklistIsOn(req.user, req.params)
+    response.success(req, res, action, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
+const getTheCardAChecklistIsOn = async (req, res, next) => {
+  try {
+    const action = await controller.getTheCardAChecklistIsOn(req.user, req.params)
+    response.success(req, res, action, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
+const getCheckitemsOnAChecklist = async (req, res, next) => {
+  try {
+    const action = await controller.getCheckitemsOnAChecklist(req.user, req.params)
+    response.success(req, res, action, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
+const getACheckitemOnAChecklist = async (req, res, next) => {
+  try {
+    const action = await controller.getACheckitemOnAChecklist(req.user, req.params)
+    response.success(req, res, action, 200)
   } catch (err) {
     response.error(req, res, err.data, err.statusCode)
   }
 }
 
 // passport.authenticate('jwt', { session: false }),
-router.get('/', passport.authenticate('jwt', { session: false }), idUserInfoById)
+router.get('/:id', passport.authenticate('jwt', { session: false }), getAChecklist)
+router.get('/checklist/:id&:field', passport.authenticate('jwt', { session: false }), getFieldOnAChecklist)
+router.get('/boardChecklist/:id', passport.authenticate('jwt', { session: false }), getTheBoardTheChecklistIsOn)
+router.get('/cardChecklist/:id', passport.authenticate('jwt', { session: false }), getTheCardAChecklistIsOn)
+router.get('/itemsChecklist/:id', passport.authenticate('jwt', { session: false }), getCheckitemsOnAChecklist)
+router.get('/checkitems/:id', passport.authenticate('jwt', { session: false }), getACheckitemOnAChecklist)
 
 module.exports = router
