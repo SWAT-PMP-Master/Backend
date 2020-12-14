0e3f9e1074a6491fcc29ac2576f54210d21d2fea
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-12-14 02:44:48 -0600

    Creating/boards from trello

--- pmp-backend/api/components/boards/controller.js
+++ pmp-backend/api/components/boards/controller.js
@@ -7,21 +7,21 @@ module.exports = (store) => {
   const query = utils().queryFn()
 
   const boardsMember = async (body, params) => {
-    const tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
     tokenKeyPair.id = params.idBoard
     const boards = JSON.parse(await trelloAuth(query).getMemberShipBoards(tokenKeyPair))
     return boards
   }
 
   const getABoard = async (body, params) => {
-    const tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
     tokenKeyPair.id = params.board
     const board = JSON.parse(await trelloAuth(query).getBoard(tokenKeyPair))
     return board
   }
 
   const getABoardField = async (body, params) => {
-    const tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
     tokenKeyPair.id = params.board
     tokenKeyPair.field = params.field
     const board = JSON.parse(await trelloAuth(query).getBoardfield(tokenKeyPair))
@@ -29,9 +29,89 @@ module.exports = (store) => {
   }
 
   const getABoardAction = async (body, params) => {
-    const tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
     tokenKeyPair.id = params.board
     const board = JSON.parse(await trelloAuth(query).getBoardAction(tokenKeyPair))
+    return board
+  }
+
+  const getACardOnABoard = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    tokenKeyPair.idCard = params.idCard
+    const board = JSON.parse(await trelloAuth(query).getACardOnABoard(tokenKeyPair))
+    return board
+  }
+
+  const getBoardsStartsOnABoard = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.boardsId = params.boardsId
+    const board = JSON.parse(await trelloAuth(query).getBoardsStartsOnABoard(tokenKeyPair))
+    return board
+  }
+
+  const getChecklistsOnABoard = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const board = JSON.parse(await trelloAuth(query).getChecklistsOnABoard(tokenKeyPair))
+    return board
+  }
+
+  const getCardsOnABoard = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const board = JSON.parse(await trelloAuth(query).getCardsOnABoard(tokenKeyPair))
+    return board
+  }
+
+  const getFilteredCardsOnABoard = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    tokenKeyPair.filter = params.filter
+    const board = JSON.parse(await trelloAuth(query).getFilteredCardsOnABoard(tokenKeyPair))
+    return board
+  }
+
+  const getCustomFieldsForBand = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const board = JSON.parse(await trelloAuth(query).getCustomFieldsForBand(tokenKeyPair))
+    return board
+  }
+
+  const getLabelsOnABoard = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const board = JSON.parse(await trelloAuth(query).getLabelsOnABoard(tokenKeyPair))
+    return board
+  }
+
+  const getFilteredListsOnAboard = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.boardId = params.boardId
+    const board = JSON.parse(await trelloAuth(query).getFilteredListsOnAboard(tokenKeyPair))
+    return board
+  }
+
+  const getTheMembersOfABoard = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    tokenKeyPair.filter = params.filter
+    const board = JSON.parse(await trelloAuth(query).getTheMembersOfABoard(tokenKeyPair))
+    return board
+  }
+
+  const getEnabledPowerupsOnBoard = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const board = JSON.parse(await trelloAuth(query).getEnabledPowerupsOnBoard(tokenKeyPair))
+    return board
+  }
+
+  const getPowerupsOnABoard = async (body, params) => {
+    let tokenKeyPair = utils().tokenKeyPairFn(body.token.trelloSecretUser)
+    tokenKeyPair.id = params.id
+    const board = JSON.parse(await trelloAuth(query).getPowerupsOnABoard(tokenKeyPair))
     return board
   }
 
@@ -39,6 +119,18 @@ module.exports = (store) => {
     boardsMember,
     getABoard,
     getABoardField,
-    getABoardAction
+    getABoardAction,
+    getACardOnABoard,
+    getBoardsStartsOnABoard,
+    getChecklistsOnABoard,
+    getCardsOnABoard,
+    getFilteredCardsOnABoard,
+    getCustomFieldsForBand,
+    getLabelsOnABoard,
+    getBoardLists,
+    getFilteredListsOnAboard,
+    getTheMembersOfABoard,
+    getEnabledPowerupsOnBoard,
+    getPowerupsOnABoard
   }
 }
--- pmp-backend/api/components/boards/network.js
+++ pmp-backend/api/components/boards/network.js
@@ -45,10 +45,121 @@ const boardsAction = async (req, res, next) => {
   }
 }
 
+const getBoardsStartsOnABoard = async (req, res, next) => {
+  try {
+    const board = await controller.getBoardsStartsOnABoard(req.user, req.params)
+    response.success(req, res, board, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
+const getChecklistsOnABoard = async (req, res, next) => {
+  try {
+    const board = await controller.getChecklistsOnABoard(req.user, req.params)
+    response.success(req, res, board, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
+const getCardsOnABoard = async (req, res, next) => {
+  try {
+    const board = await controller.getCardsOnABoard(req.user, req.params)
+    response.success(req, res, board, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
+const getFilteredCardsOnABoard = async (req, res, next) => {
+  try {
+    const board = await controller.getFilteredCardsOnABoard(req.user, req.params)
+    response.success(req, res, board, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
+const getCustomFieldsForBand = async (req, res, next) => {
+  try {
+    const board = await controller.getCustomFieldsForBand(req.user, req.params)
+    response.success(req, res, board, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
+const getLabelsOnABoard = async (req, res, next) => {
+  try {
+    const board = await controller.getLabelsOnABoard(req.user, req.params)
+    response.success(req, res, board, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
+const getBoardLists = async (req, res, next) => {
+  try {
+    const board = await controller.getBoardLists(req.user, req.params)
+    response.success(req, res, board, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
+const getFilteredListsOnAboard = async (req, res, next) => {
+  try {
+    const board = await controller.getFilteredListsOnAboard(req.user, req.params)
+    response.success(req, res, board, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
+const getTheMembersOfABoard = async (req, res, next) => {
+  try {
+    const board = await controller.getTheMembersOfABoard(req.user, req.params)
+    response.success(req, res, board, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
+const getEnabledPowerupsOnBoard = async (req, res, next) => {
+  try {
+    const board = await controller.getEnabledPowerupsOnBoard(req.user, req.params)
+    response.success(req, res, board, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
+const getPowerupsOnABoard = async (req, res, next) => {
+  try {
+    const board = await controller.getPowerupsOnABoard(req.user, req.params)
+    response.success(req, res, board, 200)
+  } catch (err) {
+    response.error(req, res, err.data, err.statusCode)
+  }
+}
+
 // passport.authenticate('jwt', { session: false }),
 router.get('/boardsMemberShip/:idBoard', passport.authenticate('jwt', { session: false }), boardsMember)
 router.get('/:board', passport.authenticate('jwt', { session: false }), boards)
 router.get('/boardsAndField/:board&:field', passport.authenticate('jwt', { session: false }), boardsField)
 router.get('/boardsAction/:board', passport.authenticate('jwt', { session: false }), boardsAction)
+router.get('/boardCards/:id&:idCard', passport.authenticate('jwt', { session: false }), getACardOnABoard)
+router.get('/boardsStarts/:boardId', passport.authenticate('jwt', { session: false }), getBoardsStartsOnABoard)
+router.get('/boardChecklists/:id', passport.authenticate('jwt', { session: false }), getChecklistsOnABoard)
+router.get('/boardCard/:id', passport.authenticate('jwt', { session: false }), getCardsOnABoard)
+router.get('/cardsFiltered/:id&:filter', passport.authenticate('jwt', { session: false }), getFilteredCardsOnABoard)
+router.get('/fieldsForBand/:id', passport.authenticate('jwt', { session: false }), getCustomFieldsForBand)
+router.get('/labels/:id', passport.authenticate('jwt', { session: false }), getLabelsOnABoard)
+router.get('/lists/:boardId', passport.authenticate('jwt', { session: false }), getBoardLists)
+router.get('/listsFiltered/:id&:filter', passport.authenticate('jwt', { session: false }), getFilteredListsOnAboard)
+router.get('/members/:id', passport.authenticate('jwt', { session: false }), getTheMembersOfABoard)
+router.get('/powerUpsOnBoard/:id', passport.authenticate('jwt', { session: false }), getEnabledPowerupsOnBoard)
+router.get('/getPowerUps/:id', passport.authenticate('jwt', { session: false }), getPowerupsOnABoard)
 
 module.exports = router
